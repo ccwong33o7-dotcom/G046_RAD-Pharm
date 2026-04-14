9b5799c3eab65b4c93c90255625965a384bd2b99
daphneee31 <daphnechow2007@gmail.com>
2026-04-14 22:57:32 +0800

    Create crafting.py

---
+++ crafting.py
@@ -0,0 +1,5 @@
+import pygame 
+
+RECIPES = {
+    "Rad-Ointment": {"Filtered water": 1, "Glowing Aloe": 1 , "Scrap Fiber": 1},
+    "Speed Serum": {"Bio-Fuel": 1, "Rusty Thorn": 1, "Filtered water": 1},
