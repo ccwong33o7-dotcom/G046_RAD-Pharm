3f73155870e0b9709b0547ad1b68176fdc78f0e4
daphneee31 <daphnechow2007@gmail.com>
2026-04-14 23:04:17 +0800

    Update crafting.py
    Added few new recipes

--- crafting.py
+++ crafting.py
@@ -3,3 +3,8 @@ import pygame
 RECIPES = {
     "Rad-Ointment": {"Filtered water": 1, "Glowing Aloe": 1 , "Scrap Fiber": 1},
     "Speed Serum": {"Bio-Fuel": 1, "Rusty Thorn": 1, "Filtered water": 1},
+    "Lung-Clear": {"Filtered water":1, "Glowing Aloe": 1},
+    "Blood-Stop":{"Scrap Fiber":1, "Rusty Thorn": 1},
+    "Pain Killer":{"Bio_Fuel":1, "Glowing Aloe": 1, "Rusty Thorn": 1}
+}
+
