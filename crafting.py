6a1dddc42fb74d2d2e9aa859b180a5c33bb8c146
daphneee31 <daphnechow2007@gmail.com>
2026-04-21 21:46:11 +0800

    Update crafting.py

--- crafting.py
+++ crafting.py
@@ -1,5 +1,8 @@
 import pygame 
 
+screen_width = 1280
+screen_height = 720
+
 RECIPES = {
     "Rad-Ointment": {"Filtered water": 1, "Glowing Aloe": 1 , "Scrap Fiber": 1},
     "Speed Serum": {"Bio-Fuel": 1, "Rusty Thorn": 1, "Filtered water": 1},
@@ -8,7 +11,8 @@ RECIPES = {
     "Pain Killer":{"Bio_Fuel":1, "Glowing Aloe": 1, "Rusty Thorn": 1}
 }
 
-inventory = {"Filtered water": "Infinite", "Bio-Fuel": "Infinite", "Scrap Fiber": "Infinite", "Glowing Aloe": 0, "Rusty Thorn":0 }
+inventory = {"Filtered water": "Infinite", "Bio-Fuel": "Infinite", "Scrap Fiber": "Infinite", "Glowing Aloe": 2, "Rusty Thorn":2 
+             ,"Rad-Ointment": 0 , "Speed Serum": 0, "Lung-Clear": 0, "Blood-Stop": 0, "Pain Killer": 0}
 
 def try_to_craft(item_name):
     recipe = RECIPES[item_name]
@@ -27,6 +31,35 @@ def try_to_craft(item_name):
 
             inventory[item_name] += 1
 
+pygame.init()
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Junkyard Lab")
+font = pygame.font.SysFont("Arial", 22)
+
+running=True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((30, 30, 30))
+    
+for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        running = False
+
+    if event.type == pygame.KEYDOWN:
+        if event .key == pygame.K_1:
+            try_to_craft("Rad-Ointment")
+        if event.key == pygame.K_2:
+            try_to_craft("Speed Serum")
+        if event.key == pygame.K_3:
+            try_to_craft("Lung-Clear")
+        if event.key == pygame.K_4:
+            try_to_craft("Blood-Stop")
+        if event.key == pygame.K_5:
+            try_to_craft("Pain Killer")
+    
             
 
 
