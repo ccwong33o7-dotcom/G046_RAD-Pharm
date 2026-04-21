a3a72b7d2fb6991f9c132c91353c3ab8012e6d46
Kaiqi0713 <zhangkq0113@gmail.com>
2026-04-21 20:20:37 +0800

    Color definition

--- greenhouse.py
+++ greenhouse.py
@@ -1,6 +1,17 @@
 import pygame
 import sys
 
+pygame.init()
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Hazard Greenhouse Prototype")
+clock = pygame.time.Clock()
+
+COLOR_BG = (30, 30, 30)
+COLOR_PLANT= (50, 200, 50)
+COLOR_DUST = (150, 150, 100)
+COLOR_TEXT = (200, 200, 200)
+
 class Plant:
     def __init__(self, x, y):
         self.rect = pygame.Rect(x, y, 60, 100)
@@ -22,13 +33,7 @@ class Plant:
         if self.dust < 0: 
             self.dust = 0
 
-pygame.init()
-screen = pygame.display.set_mode((600, 400))
-pygame.display.set_caption("Hazard Greenhouse - Survival Alpha")
-clock = pygame.time.Clock() 
 
-
-my_plant = Plant(270, 250)
 
 
 
