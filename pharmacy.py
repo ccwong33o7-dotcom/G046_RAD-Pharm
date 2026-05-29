6dd452613884a276e2bc3ab93a9706e009bb41b5
Kaiqi0713 <zhangkq0113@gmail.com>
2026-05-29 13:36:13 +0800

    Update pharmacy.py

--- pharmacy.py
+++ pharmacy.py
@@ -7,8 +7,5 @@ def draw_pharmacy(screen, bg_img):
     else:
         screen.fill((255,250,200))
 
-    greenhouse_btn_rect = pygame.Rect(1180,130,80,40)
-    crafting_btn_rect = pygame.Rect(1180,180,80,40)
-    shop_btn_rect = pygame.Rect(1180,230,80,40) 
    
-    return greenhouse_btn_rect, crafting_btn_rect, shop_btn_rect
+    return True
