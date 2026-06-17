2d2764fc998253127af395bf683b0ff2cd53e286
Kaiqi0713 <zhangkq0113@gmail.com>
2026-06-17 10:40:55 +0800

    Update map.py

--- map.py
+++ map.py
@@ -5,8 +5,9 @@ def draw_map(screen, map_bg_img, font, mouse_pos, img_greenhouse, img_shop, img_
         screen.blit(map_bg_img, (0, 0))
     else:
        screen.fill((50, 50, 50))
-       error_txt = font.render("Map background not found", True, (255, 0, 0))
-       screen.blit(error_txt, (1280 // 2 - 150, 720 // 2))
+       if font:
+           error_txt = font.render("Map background not found", True, (255, 0, 0))
+           screen.blit(error_txt, (1280 // 2 - 150, 720 // 2))
     
     shop_rect = pygame.Rect(180, 130, 260, 190)
     greenhouse_rect = pygame.Rect(820, 140, 270, 200)
@@ -22,18 +23,26 @@ def draw_map(screen, map_bg_img, font, mouse_pos, img_greenhouse, img_shop, img_
     if img_lab:
         screen.blit(img_lab, (lab_rect.x, lab_rect.y))
     
-    if shop_rect.collidepoint(mouse_pos):
-        label = font.render("Shop", True, (240, 190, 60))
-        screen.blit(label, (shop_rect.x + 30, shop_rect.y - 40))
-    elif greenhouse_rect.collidepoint(mouse_pos):
-        label = font.render("Greenhouse", True, (240, 190, 60))
-        screen.blit(label, (greenhouse_rect.x + 30, greenhouse_rect.y - 40))
-    elif pharmacy_rect.collidepoint(mouse_pos):
-        label = font.render("Pharmacy", True, (255, 100, 100))
-        screen.blit(label, (pharmacy_rect.x + 10, pharmacy_rect.y - 40))
-    elif lab_rect.collidepoint(mouse_pos):
-        label = font.render("Junkyard Lab", True, (150, 200, 255))
-        screen.blit(label, (lab_rect.x + 20, lab_rect.y - 40))
+
+    if font:
+        small_bold_font = pygame.font.SysFont("Arial", 25, bold=True)
+        TEXT_COLOR = (245, 245, 220)
+        
+        
+        if shop_rect.collidepoint(mouse_pos):
+            label = small_bold_font.render("Shop", True, TEXT_COLOR)
+            screen.blit(label, (shop_rect.x + 30, shop_rect.y - 40))
+        elif greenhouse_rect.collidepoint(mouse_pos):
+            label = small_bold_font.render("Greenhouse", True, TEXT_COLOR)
+            screen.blit(label, (greenhouse_rect.x + 30, greenhouse_rect.y - 40))
+        elif pharmacy_rect.collidepoint(mouse_pos):
+            label = small_bold_font.render("Pharmacy", True, TEXT_COLOR)
+            screen.blit(label, (pharmacy_rect.x + 10, pharmacy_rect.y - 40))
+        elif lab_rect.collidepoint(mouse_pos):
+            label = small_bold_font.render("Junkyard Lab", True, TEXT_COLOR)
+            screen.blit(label, (lab_rect.x + 20, lab_rect.y - 40))
+
+        font.set_bold(False)
 
     return greenhouse_rect, shop_rect, lab_rect, pharmacy_rect
 
